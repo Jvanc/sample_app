e12307a58b3b1cf5bb68bc46a8efbfea91b565ac
Jacky <jackyvanc@gmail.com>
2013-06-10 22:46:39 -0700

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,12 @@ SampleApp::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+  SampleApp::Application.routes.draw do
+    match '/contact', :to => 'pages#contact'
+    match '/about', :to => 'pages#about'
+    match '/help', :to => 'pages#help'
+    root :to => 'pages#home'
+  end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
